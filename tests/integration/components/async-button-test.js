70f35635bcc662088a25ee009b66d9793ca4096c
Muyao Xu <muyaoxu@usc.edu>
2018-12-06 22:12:23 -0800

    finished four rendering tests

--- tests/integration/components/async-button-test.js
+++ tests/integration/components/async-button-test.js
@@ -1,13 +1,15 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import RSVP from 'rsvp';
+import { later } from '@ember/runloop';
 
 module('Integration | Component | async-button', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    
+
     await render(hbs`{{async-button}}`);
 
     assert.equal(this.element.textContent.trim(), '');
@@ -20,5 +22,36 @@ module('Integration | Component | async-button', function(hooks) {
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
+  test('on initial render', async function(assert) {
+    this.set('defaultText', 'Save');
+
+    await render(hbs`<AsyncButton
+      data-test="restaurant-create"
+      @defaultText={{defaultText}}
+    />`);
+
+    assert.dom('[data-test="restaurant-create"]').hasText('Save');
+  });
+
+  test('onClick render', async function(assert) {
+    this.set('defaultText', 'Save');
+    this.set('onClick', function() {
+      return new RSVP.Promise((resolve) => {
+        later(() => {
+          resolve();
+        }, 1000);
+      });
+    })
+
+    await render(hbs`<AsyncButton
+      data-test="restaurant-create"
+      @defaultText={{defaultText}}
+      @onClick={{onClick}}
+    />`);
+
+    await click('[data-test="restaurant-create"]');
+    assert.dom('[data-test="restaurant-create"]').hasText('Save...');
   });
 });
